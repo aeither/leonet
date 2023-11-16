cdf0d1fd9612eb8843654d90746e10e82098f71c
aeither <36173828+aeither@users.noreply.github.com>
2023-11-16 19:45:40 +0300

    prep sign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,93 +2,135 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
+import { PrivateKey } from "@aleohq/sdk";
 
 export default function Home() {
-    const [account, setAccount] = useState(null);
-    const [executing, setExecuting] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [executing, setExecuting] = useState(false);
+  const [signingAccount, setSigningAccount] = useState<PrivateKey | null>(null);
+  const [signingKey, setSigningKey] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
+  const textEncoder = new TextEncoder();
 
-    const generateAccount = async () => {
-        workerRef.current?.postMessage("key");
+  const onKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSigningAccount(null);
+    try {
+      setSigningAccount(PrivateKey.from_string(event.target.value));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSigningKey(null);
+      setMessage(null);
+    }
+  };
+  const signString = (str: string) => {
+    if (str === "" || signingAccount === null) return;
+    return signingAccount.sign(textEncoder.encode(str)).to_string();
+  };
+  const onMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.value) {
+      alert("no message");
+      return;
+    }
+    setMessage(event.target.value);
+
+    try {
+      setSigningKey(signString(event.target.value) || null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const generateAccount = async () => {
+    workerRef.current?.postMessage("key");
+  };
+
+  async function execute() {
+    setExecuting(true);
+    workerRef.current?.postMessage("execute");
+  }
+
+  const workerRef = useRef<Worker>();
+
+  interface AleoWorkerMessageEvent {
+    type: string;
+    result: any;
+  }
+
+  useEffect(() => {
+    workerRef.current = new Worker(new URL("worker.ts", import.meta.url));
+    workerRef.current.onmessage = (
+      event: MessageEvent<AleoWorkerMessageEvent>
+    ) => {
+      if (event.data.type === "key") {
+        setAccount(event.data.result);
+      } else if (event.data.type === "execute") {
+        setExecuting(false);
+      }
+      alert(`WebWorker Response => ${event.data.result}`);
     };
+    return () => {
+      workerRef.current?.terminate();
+    };
+  }, []);
 
-    async function execute() {
-        setExecuting(true);
-        workerRef.current?.postMessage("execute");
-    }
+  const handleWork = useCallback(async () => {
+    workerRef.current?.postMessage("execute");
+  }, []);
 
-    const workerRef = useRef<Worker>();
+  return (
+    <main className={styles.main}>
+      <div>
+        <input
+          name="privateKey"
+          placeholder="Private Key"
+          onChange={onKeyChange}
+        />
+      </div>
 
-    interface AleoWorkerMessageEvent {
-        type: string;
-        result: any;
-    }
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+      </div>
 
-    useEffect(() => {
-        workerRef.current = new Worker(new URL("worker.ts", import.meta.url));
-        workerRef.current.onmessage = (
-            event: MessageEvent<AleoWorkerMessageEvent>
-        ) => {
-            if (event.data.type === "key") {
-                setAccount(event.data.result);
-            } else if (event.data.type === "execute") {
-                setExecuting(false);
-            }
-            alert(`WebWorker Response => ${event.data.result}`);
-        };
-        return () => {
-            workerRef.current?.terminate();
-        };
-    }, []);
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className={styles.logo}
+          src="/aleo.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={45}
+          priority
+        />
+      </div>
 
-    const handleWork = useCallback(async () => {
-        workerRef.current?.postMessage("execute");
-    }, []);
-
-    return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>src/app/page.tsx</code>
-                </p>
-            </div>
-
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-                <Image
-                    className={styles.logo}
-                    src="/aleo.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={45}
-                    priority
-                />
-            </div>
-
-            <div className={styles.card}>
-                <p>
-                    <button onClick={generateAccount}>
-                        {account
-                            ? `Account is ${JSON.stringify(account)}`
-                            : `Click to generate account`}
-                    </button>
-                </p>
-                <p>
-                    <button disabled={executing} onClick={execute}>
-                        {executing
-                            ? `Executing...check console for details...`
-                            : `Execute helloworld.aleo`}
-                    </button>
-                </p>
-            </div>
-        </main>
-    );
+      <div className={styles.card}>
+        <p>
+          <button onClick={generateAccount}>
+            {account
+              ? `Account is ${JSON.stringify(account)}`
+              : `Click to generate account`}
+          </button>
+        </p>
+        <p>
+          <button disabled={executing} onClick={execute}>
+            {executing
+              ? `Executing...check console for details...`
+              : `Execute helloworld.aleo`}
+          </button>
+        </p>
+      </div>
+    </main>
+  );
 }
