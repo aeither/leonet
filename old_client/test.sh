91e4b97311fa5337b6db969afc58cc3277b788d1
aeither <36173828+aeither@users.noreply.github.com>
2023-11-17 17:32:56 +0300

    clean demo

--- old_client/test.sh
+++
@@ -1,8 +0,0 @@
-echo 'testing...' 
-
-cd leonet_program
-
-output=$(leo run update_score  "[ "72u8", "101u8", "108u8", "108u8", "111u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8", "0u8" ]" 0u8 123field 12u64)
-
-echo "The output is"
-echo $output
